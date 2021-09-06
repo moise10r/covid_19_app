f778bc699b824961f033f0f02890fdd7e4a25a48
moise10r <moiserushanika2006@gmail.com>
2021-09-07 00:24:58 +0200

    Add detail page layout

--- src/component/features/DetailPage/detail.jsx
+++ src/component/features/DetailPage/detail.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import TopBar from '../../common/jsx/TopBar/topBar';
+import continentMap from '../HomePage/europ_map.png';
 
 const Detail = () => (
-  <div>
-    details
-  </div>
+  <>
+    <TopBar />
+    <div className="main-home-section">
+      <div className="main-home-wrapper">
+        <div className="continent-container">
+          <div className="left-container">
+            <div className="img-wrapper">
+              <div className="bg" />
+              <img src={continentMap} alt="continent-img" />
+            </div>
+          </div>
+          <div className="right-container">
+            <div className="content-wrapper">
+              <h1>Europe</h1>
+              <span>28339</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default Detail;
--- src/component/features/HomePage/home.jsx
+++ src/component/features/HomePage/home.jsx
@@ -4,151 +4,155 @@ import { BiRightArrowCircle } from 'react-icons/bi';
 import './home.scss';
 import continent from './europe.png';
 import continentMap from './europ_map.png';
+import TopBar from '../../common/jsx/TopBar/topBar';
 
 const Home = () => (
-  <div className="main-home-section">
-    <div className="main-home-wrapper">
-      <div className="continent-container">
-        <div className="left-container">
-          <div className="img-wrapper">
-            <div className="bg" />
-            <img src={continent} alt="continent-img" />
+  <>
+    <TopBar />
+    <div className="main-home-section">
+      <div className="main-home-wrapper">
+        <div className="continent-container">
+          <div className="left-container">
+            <div className="img-wrapper">
+              <div className="bg" />
+              <img src={continent} alt="continent-img" />
+            </div>
+          </div>
+          <div className="right-container">
+            <div className="content-wrapper">
+              <h1>Europe</h1>
+              <span>28339</span>
+            </div>
           </div>
         </div>
-        <div className="right-container">
-          <div className="content-wrapper">
-            <h1>Europe</h1>
-            <span>28339</span>
-          </div>
+        <div className="countries-container">
+          <h2 className="title">STATS BY COUNTRY</h2>
+          <ul className="countries-list">
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+            <li className="country">
+              <div className="img-wrapper">
+                <div className="bg" />
+                <img src={continentMap} alt="continent-img" />
+              </div>
+              <div className="content">
+                <h2 className="name">France</h2>
+                <span className="cases">3346</span>
+              </div>
+              <span className="extend">
+                <IconContext.Provider value={{ className: 'icon ' }}>
+                  <BiRightArrowCircle />
+                </IconContext.Provider>
+              </span>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="countries-container">
-        <h2 className="title">STATS BY COUNTRY</h2>
-        <ul className="countries-list">
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-          <li className="country">
-            <div className="img-wrapper">
-              <div className="bg" />
-              <img src={continentMap} alt="continent-img" />
-            </div>
-            <div className="content">
-              <h2 className="name">France</h2>
-              <span className="cases">3346</span>
-            </div>
-            <span className="extend">
-              <IconContext.Provider value={{ className: 'icon ' }}>
-                <BiRightArrowCircle />
-              </IconContext.Provider>
-            </span>
-          </li>
-        </ul>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
