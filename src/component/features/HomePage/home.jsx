b5c925ac95c4cf63200e4c438b049c82700f53af
moise10r <moiserushanika2006@gmail.com>
2021-09-06 15:06:31 +0200

    Add home continent layout

--- src/component/features/HomePage/home.jsx
+++ src/component/features/HomePage/home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './home.scss';
+import continent from './europe.png';
 
 const Home = () => (
   <div className="main-home-section">
-    Home
+    <div className="continent-container">
+      <div className="left-container">
+        <div className="img-wrapper">
+          <img src={continent} alt="continent-img" />
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="content-wrapper">
+          <h1>Europe</h1>
+          <span>28339</span>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
