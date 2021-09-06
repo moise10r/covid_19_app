30c4051ac29c2526ca16c705f56d93882ac70a80
moise10r <moiserushanika2006@gmail.com>
2021-09-06 10:48:50 +0200

    Add top bar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
+import TopBar from './component/common/jsx/topBar';
 import Home from './component/features/HomePage/home';
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
---
+++ src/component/common/jsx/topBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TopBar = () => (
+  <div className="top-navbar">
+    Top
+  </div>
+);
+
+export default TopBar;
