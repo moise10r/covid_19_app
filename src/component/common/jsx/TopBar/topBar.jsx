b18e46c71511add7801ca275b2eb0c6a50a07528
moise10r <moiserushanika2006@gmail.com>
2021-09-06 13:46:33 +0200

    Add main wrapper on the topbar container

--- src/component/common/jsx/TopBar/topBar.jsx
+++ src/component/common/jsx/TopBar/topBar.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { IconContext } from 'react-icons';
-import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowBack, IoIosSettings } from 'react-icons/io';
+import { FaMicrophone } from 'react-icons/fa';
+import './topBar.scss';
 
 const TopBar = () => (
   <div className="top-navbar">
-    <div className="left-container">
-      <span>
-        <IconContext.Provider value={{ className: 'back' }}>
-          <IoIosArrowBack />
-        </IconContext.Provider>
-      </span>
-      <span>2021</span>
-    </div>
-    <div className="center-container">
-      <span>most views</span>
-    </div>
-    <div className="right-container">
-      <span>
-        <IconContext.Provider value={{ className: 'back' }}>
-          <IoIosArrowBack />
-        </IconContext.Provider>
-      </span>
-      <span>
-        <IconContext.Provider value={{ className: 'back' }}>
-          <IoIosArrowBack />
-        </IconContext.Provider>
-      </span>
+    <div className="main-wrapper">
+      <div className="left-container">
+        <span>
+          <IconContext.Provider value={{ className: 'back' }}>
+            <IoIosArrowBack />
+          </IconContext.Provider>
+        </span>
+        <span>2021</span>
+      </div>
+      <div className="center-container">
+        <span>most views</span>
+      </div>
+      <div className="right-container">
+        <span>
+          <IconContext.Provider value={{ className: 'back' }}>
+            <FaMicrophone />
+          </IconContext.Provider>
+        </span>
+        <span>
+          <IconContext.Provider value={{ className: 'back' }}>
+            <IoIosSettings />
+          </IconContext.Provider>
+        </span>
+      </div>
     </div>
   </div>
 );
