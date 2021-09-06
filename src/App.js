480b6f6afa212486edeae691a91d71100d38b5ec
moise10r <moiserushanika2006@gmail.com>
2021-09-07 00:12:55 +0200

    Add detail page router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import TopBar from './component/common/jsx/TopBar/topBar';
 import Home from './component/features/HomePage/home';
 import './app.scss';
+import Detail from './component/features/DetailPage/detail';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <TopBar />
       <Switch>
         <Route path="/" component={Home} />
+        <Route path="/detail" component={Detail} />
       </Switch>
     </div>
   );
