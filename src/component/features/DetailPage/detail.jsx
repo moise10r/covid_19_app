d962d7f2a46d3f79b3263da218923e083540478c
moise10r <moiserushanika2006@gmail.com>
2021-09-07 09:41:35 +0200

    Add town section style

--- src/component/features/DetailPage/detail.jsx
+++ src/component/features/DetailPage/detail.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { BiRightArrowCircle } from 'react-icons/bi';
 import TopBar from '../../common/jsx/TopBar/topBar';
 import continentMap from '../HomePage/europ_map.png';
 import './detail.scss';
@@ -21,6 +23,24 @@ const Detail = () => (
               <span>28339</span>
             </div>
           </div>
+        </div>
+        <div className="towns-container">
+          <h2 className="title">CITY/TOWN BREAKDOWN - 2021</h2>
+          <ul className="towns-list">
+            <li className="town">
+              <div className="left-container">
+                <h2 className="name">France</h2>
+              </div>
+              <div className="right-container">
+                <span>453 cases</span>
+                <span className="extend">
+                  <IconContext.Provider value={{ className: 'icon ' }}>
+                    <BiRightArrowCircle />
+                  </IconContext.Provider>
+                </span>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
