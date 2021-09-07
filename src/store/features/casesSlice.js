dfe50745aae62df9d60cce59e7868d8a2a8415af
moise10r <moiserushanika2006@gmail.com>
2021-09-07 14:30:18 +0200

    Add create slice reducer

---
+++ src/store/features/casesSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseURL = 'https://covid-api.mmediagroup.fr/v1/cases/';
+const getCasesByContinent = createAsyncThunk('/cases/continent', async () => {
+  const { data } = await axios.get(`${baseURL}?continent=europe`);
+  return data;
+});
+
+const casesSlice = createSlice({
+  name: 'cases',
+  initialState: {
+    cases: [],
+    status: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getCasesByContinent.pending, (state) => {
+      state.status = 'loading';
+    });
+    builder.addCase(getCasesByContinent.fulfilled, (state, action) => {
+      state.status = 'success';
+      state.cases = action.payload;
+    });
+    builder.addCase(getCasesByContinent.rejected, (state) => {
+      state.status = 'failed';
+    });
+  },
+});
+
+export default casesSlice.reducer;
