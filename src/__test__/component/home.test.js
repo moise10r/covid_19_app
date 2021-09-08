8ff41f4d561b17fd6dcf7c79d29cb0816dbb4d7d
moise10r <moiserushanika2006@gmail.com>
2021-09-08 19:56:06 +0200

    Mock router in the Home component

--- src/__test__/component/home.test.js
+++ src/__test__/component/home.test.js
@@ -1,13 +1,16 @@
 import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from '../../component/features/HomePage/home';
 import store from '../../store/store';
 
 const MockHome = () => (
-  <Provider store={store}>
-    <Home />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  </BrowserRouter>
 );
 
 describe('Test the Home component', () => {
