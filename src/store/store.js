5ef3a71b66e1fa6a89bb5c01d5756d650878cb95
moise10r <moiserushanika2006@gmail.com>
2021-09-09 21:36:34 +0200

    Remove logger middleware.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import reducer from './features/casesSlice';
 
 const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
