5d2cc285161389246a6b0c662f66d4f40e19a69f
moise10r <moiserushanika2006@gmail.com>
2021-09-09 20:01:55 +0200

    Add the filterByContinent layout

--- src/component/common/jsx/Filter/filterByContinent.jsx
+++ src/component/common/jsx/Filter/filterByContinent.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import continents from '../../../../services/continentList';
 
 const FilterByContinent = () => (
-  <div>
-    hello
+  <div className="continents-container">
+    <ul className="continent-list">
+      {
+        continents.map(({ id, name }) => (
+          <li key={id} className="continent">{name}</li>
+        ))
+      }
+    </ul>
   </div>
 );
 
