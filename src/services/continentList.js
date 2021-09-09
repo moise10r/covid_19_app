35a73b20044d92599cd3c40a88bbba94bc4e0055
moise10r <moiserushanika2006@gmail.com>
2021-09-09 19:56:26 +0200

    Add continents service list

---
+++ src/services/continentList.js
@@ -0,0 +1,9 @@
+const continents = [
+  { id: 1, name: 'Africa' },
+  { id: 2, name: 'America' },
+  { id: 3, name: 'Asia' },
+  { id: 4, name: 'Europe' },
+  { id: 5, name: '' },
+];
+
+export default continents;
